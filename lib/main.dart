08afdc5af47215974edf29c5604d4a7a8503ddc5
mujtaba193 <mujtabatoby2b@gmail.com>
2024-02-13 18:44:43 +0300

    add map

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:map_project/map_screen.dart';
+import 'package:map_project/map_screen2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: MapSample(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
