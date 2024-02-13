ceddd25ee6add8e05cbd030b70ff2595bce5306b
mujtaba193 <mujtabatoby2b@gmail.com>
2024-02-13 18:25:54 +0300

    m

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -14,8 +14,7 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
+  Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
   BitmapDescriptor iconPic = BitmapDescriptor.defaultMarker;
 
   final TextEditingController searchController = TextEditingController();
@@ -30,6 +29,19 @@ class MapSampleState extends State<MapSample> {
       markerId: MarkerId('_kGooglePlex1'),
       icon: BitmapDescriptor.defaultMarker,
       position: LatLng(60.003750, 30.287770),
+      infoWindow: InfoWindow(title: 'dormitory') //37.431542, -122.095130
+      );
+  final Marker googleMarkerOrigin = Marker(
+      markerId: MarkerId('_path'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+      position: LatLng(37.382890, -122.045861),
+      infoWindow: InfoWindow(title: 'dormitory') //37.431542, -122.095130
+      );
+
+  final Marker googleMarkerDestination = Marker(
+      markerId: MarkerId('_path'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+      position: LatLng(37.374969, -121.908777),
       infoWindow: InfoWindow(title: 'dormitory') //37.431542, -122.095130
       );
   final Marker googleMarker2 = Marker(
@@ -109,6 +121,50 @@ class MapSampleState extends State<MapSample> {
     return locationData;
   }
 
+  /*Future<Map<String, dynamic>> getPath(liveMarker, googleMarker1) async {
+    final String url =
+        'http://maps.googleapis.com/maps/api/directions/json?origin=$liveMarker&destination=$googleMarkerpath&key=AIzaSyD_EXNQjjvmLVJE37nA8zVQdTPRDcQStYE';
+    var respo = await http.get(Uri.parse(url));
+    var json = jsonDecode(respo.body);
+    var result = {
+      'bounds_ne': json['routes'][0]['bounds']['northeast'],
+      'bounds_sw': json['routes'][0]['bounds']['northeast'],
+      'start_location': json['routes'][0]['legs'][0]['start_location'],
+      'end_location': json['routes'][0]['legs'][0]['end_location'],
+      'polyline': json['routes'][0]['overview_polyline']['points'],
+    };
+    print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh$result');
+    return result;
+  }*/
+  Map<PolylineId, Polyline> _mapPolylines = {};
+  int _polylineIdCounter = 1;
+
+  void _add() {
+    final String polylineIdVal = 'polyline_id_$_polylineIdCounter';
+    _polylineIdCounter++;
+    final PolylineId polylineId = PolylineId(polylineIdVal);
+
+    final Polyline polyline = Polyline(
+      polylineId: polylineId,
+      consumeTapEvents: true,
+      color: Colors.red,
+      width: 5,
+      points: _createPoints(),
+    );
+
+    setState(() {
+      _mapPolylines[polylineId] = polyline;
+    });
+  }
+
+  List<LatLng> _createPoints() {
+    final List<LatLng> points = <LatLng>[];
+    points.add(LatLng(37.382890, -122.045861));
+    points.add(LatLng(37.374969, -121.908777));
+
+    return points;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -165,6 +221,8 @@ class MapSampleState extends State<MapSample> {
                         ...googleMarkerList ?? [googleMarker1],
                         googleMarker ?? googleMarker1,
                         googleMarker1,
+                        googleMarkerOrigin,
+                        googleMarkerDestination,
                         googleMarker2,
                         liveMarker ?? googleMarker1,
                       },
@@ -172,9 +230,7 @@ class MapSampleState extends State<MapSample> {
                           target: LatLng(lat ?? 55.78619841559829,
                               longt ?? 37.550435740210204),
                           zoom: 12.0),
-                      polylines: {
-                        Polyline(polylineId: PolylineId("rout"))
-                      }, // 55.78619841559829, 37.550435740210204
+                      polylines: Set<Polyline>.of(_mapPolylines.values),
                       onMapCreated: (GoogleMapController controller) {
                         _controller.complete(controller);
                       },
@@ -210,8 +266,9 @@ class MapSampleState extends State<MapSample> {
                 icon: BitmapDescriptor.defaultMarkerWithHue(
                     BitmapDescriptor.hueYellow),
                 position: LatLng(lat!, longt!),
-                infoWindow: InfoWindow(title: 'work') //37.431542, -122.095130
-                );
+                infoWindow: InfoWindow(title: 'work'));
+            //getPath(liveMarker, googleMarker1);
+            _add();
           },
           child: const Icon(
             Icons.navigation,
